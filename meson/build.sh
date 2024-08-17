e5d9aca47e276ad50d5be9ce016afe6e947622fd
arthurdead <arthur.kinder@gmail.com>
2024-08-17 17:40:40 -0300

    add support for install in meson

--- meson/build.sh
+++ meson/build.sh
@@ -3,6 +3,7 @@
 builddir=./'build_linux'
 src_root_dir=$(realpath ./../'src')
 src_engine_dir=~/'.steam/steam/steamapps/common/Source SDK Base 2013 Multiplayer'
+game_dir=$(realpath ./../../'the-heist-files')
 
 if [[ -d "$builddir" ]]; then
 	if [[ ! -f "$builddir/build.ninja" ]]; then
@@ -11,7 +12,7 @@ if [[ -d "$builddir" ]]; then
 fi
 
 if [[ ! -d "$builddir" ]]; then
-	meson setup --cross-file ./'x86-linux-gnu' "$builddir" --backend='ninja' -Dsrc_root_dir="$src_root_dir" -Dsrc_engine_dir="$src_engine_dir"
+	meson setup --cross-file ./'x86-linux-gnu' "$builddir" --backend='ninja' -Dsrc_root_dir="$src_root_dir" -Dsrc_engine_dir="$src_engine_dir" -Dgame_dir="$game_dir"
 	if [[ $? != 0 ]]; then
 		exit 1
 	fi
@@ -23,6 +24,11 @@ if [[ ! -d "$builddir" ]]; then
 fi
 
 meson compile -j 8 -C "$builddir"
+if [[ $? != 0 ]]; then
+	exit 1
+fi
+
+meson install -C "$builddir" --no-rebuild --skip-subprojects='webm'
 if [[ $? != 0 ]]; then
 	exit 1
 fi
